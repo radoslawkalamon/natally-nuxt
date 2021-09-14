317d91ad8377de8efe2647bb7c23e5e3974377f0
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-14 20:51:03 +0200

    feat: [#2] Logo component

---
+++ components/components/Logo/Logo.stories.ts
@@ -0,0 +1,13 @@
+import Logo from './Logo.vue';
+
+export default {
+  title: 'Components/Logo',
+  component: Logo,
+};
+
+export const Default = () => ({
+  components: {
+    Logo
+  },
+  template: '<Logo />',
+});
