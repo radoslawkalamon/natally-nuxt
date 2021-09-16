6b429e620673cac9c478965c721d6a5f8ea78efa
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-16 22:01:04 +0200

    feat: nuxt/image

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxt/image',
   ],
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
