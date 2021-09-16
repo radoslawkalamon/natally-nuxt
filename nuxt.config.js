cc25575dbc23c29243bcaaa865c833fc86e63510
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-16 19:01:48 +0200

    feat: [#66] Base variables and styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,12 +19,16 @@ export default {
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/styles/variables.css'
+    '@/assets/styles/variables.css',
+    '@/assets/styles/schemas.css',
+    '@/assets/styles/body.css',
   ],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    { path: '~/components', extensions: ['vue'] }
+  ],
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
