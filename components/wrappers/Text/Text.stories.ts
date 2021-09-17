b09883678bb1ddf6f9e3d3ad1a4e958c73415d9a
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-17 21:41:31 +0200

    feat: [#3] Text component

---
+++ components/wrappers/Text/Text.stories.ts
@@ -0,0 +1,36 @@
+import TextBase from './Text.vue'
+
+export default {
+  title: 'Wrappers / Text',
+  component: TextBase,
+  argTypes: {
+    default: {
+      description: '`<h1>`, `<h2>`, `<h3>`, `<h4>` & `<p>` gets design systems styling',
+      table: {
+        type: null,
+        defaultValue: null
+      }
+    }
+  }
+}
+
+export const Default = () => ({
+  components: {
+    TextBase
+  },
+  template: `<TextBase>
+    <h1>Header 1</h1>
+    <h2>Header 2</h2>
+    <h3>Header 3</h3>
+    <h4>Header 4</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices pulvinar ligula, sit amet dignissim turpis molestie malesuada. Mauris cursus velit augue, at lobortis ex sagittis nec. Cras ac pulvinar est, nec varius risus. Donec feugiat, tellus ac mattis sodales, nisi tortor congue lorem, nec varius magna lorem quis nibh. Donec lacinia, ligula quis rutrum ornare, urna nulla varius enim, eu venenatis orci lacus id tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eu quam nibh. Morbi in volutpat eros. Vivamus pulvinar non nulla ut imperdiet. Etiam accumsan aliquet vehicula.</p>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ul>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ol>
+  </TextBase>`
+})
---
+++ components/wrappers/Text/Text.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="text"
+    data-test="wrappers-text"
+  >
+    <slot />
+  </div>
+</template>
+
+<script src="./Text.ts" lang="ts"></script>
+<style src="./Text.css" scoped></style>
