bbdbf7e860e970c271f6c25c3a57e0f144ca61c0
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-30 14:44:50 +0200

    feat: common tests merge lodash defaultOptions

--- utils/commonTestSpecs.ts
+++ utils/commonTestSpecs.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import { mount } from '@vue/test-utils'
+import { merge } from 'lodash'
 
 export function shallRender (component: typeof Vue, dataTestID: string, defaultOptions?: object) {
   test('shall render', () => {
@@ -12,25 +13,25 @@ export function shallRender (component: typeof Vue, dataTestID: string, defaultO
 export function shallHaveSlot (component: typeof Vue, slot: string, defaultOptions?: object) {
   test(`shall have slot '${slot}'`, () => {
     const text = `This is slot '${slot}' test.`
-    const wrapper = mount(component, {
-      ...defaultOptions,
+    const wrapperOptions = merge(defaultOptions, {
       slots: {
         [slot]: text
       }
     })
+    const wrapper = mount(component, wrapperOptions)
     expect(wrapper.html()).toContain(text)
   })
 }
 
 export function shallHaveStringProp (component: typeof Vue, propName: string, defaultOptions?: object) {
-  test(`shall have string prop '${propName}''`, () => {
+  test(`shall have string prop '${propName}'`, () => {
     const text = `This is prop ${propName} test.`
-    const wrapper = mount(component, {
-      ...defaultOptions,
+    const wrapperOptions = merge(defaultOptions, {
       propsData: {
         [propName]: text
       }
     })
+    const wrapper = mount(component, wrapperOptions)
     expect(wrapper.html()).toContain(text)
   })
 }
