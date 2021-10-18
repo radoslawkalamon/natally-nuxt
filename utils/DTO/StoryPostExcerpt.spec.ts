0b1c1541b132f3da3826283410638f62f9cffd7b
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-10-18 16:45:21 +0200

    feat: [#9] story post excerpt DTO

---
+++ utils/DTO/StoryPostExcerpt.spec.ts
@@ -0,0 +1,34 @@
+import StoryPostExcerpt from '@/utils/DTO/StoryPostExcerpt'
+
+jest.mock('@/utils/formatters/datePublished', () => () => 'Mocked date formatter')
+jest.mock('@/utils/formatters/readingTime', () => () => 'Mocked time formatter')
+
+describe('Utils / DTO / Poem Post Excerpt', () => {
+  let storyPostExcerpt: StoryPostExcerpt
+
+  beforeAll(() => {
+    storyPostExcerpt = new StoryPostExcerpt({
+      id: '123',
+      coverImage: '/cover-image-url.webp',
+      coverImage2x: '/cover-image-url-2x.webp',
+      datePublished: 1634302144000,
+      isAudiobook: true,
+      readingTime: 10,
+      title: 'Test title',
+      url: '/test-url'
+    })
+  })
+
+  test('shall format data properly', () => {
+    expect(storyPostExcerpt).toMatchSnapshot({
+      id: '123',
+      coverImage: '/cover-image-url.webp',
+      coverImage2x: '/cover-image-url-2x.webp',
+      datePublished: 'Mocked date formatter',
+      isAudiobook: true,
+      readingTime: 'Mocked time formatter',
+      title: 'Test title',
+      url: '/test-url'
+    })
+  })
+})
