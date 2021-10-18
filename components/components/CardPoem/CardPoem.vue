0540ecee56d2296b5341d58fe644aa229ba4a80c
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-10-18 19:37:50 +0200

    feat: [#108] card poem semantics

--- components/components/CardPoem/CardPoem.vue
+++ components/components/CardPoem/CardPoem.vue
@@ -1,26 +1,37 @@
 <template>
-  <NuxtLink
+  <article
     class="card-poem"
     data-test="components-card-poem"
-    :to="url"
   >
-    <ComponentsCoverPoem
-      class="card-poem__cover"
-      :image="coverImage"
-    />
-    <div class="card-poem__content">
-      <ComponentsTitle
-        class="card-poem__title"
-        :title="title"
-        :type="3"
-        :shall-show-underscore="false"
+    <NuxtLink
+      class="card-poem__link"
+      :to="url"
+    >
+      <ComponentsCoverPoem
+        class="card-poem__cover"
+        :image="coverImage"
       />
-      <ComponentsTextMeta
-        class="card-poem__meta"
-        :text="datePublished"
-      />
-    </div>
-  </NuxtLink>
+      <div class="card-poem__content">
+        <header class="card-poem__header">
+          <ComponentsTitle
+            class="card-poem__title"
+            :design="3"
+            :title="title"
+            :type="1"
+            :shall-show-underscore="false"
+          />
+        </header>
+        <footer class="card-poem__footer">
+          <ComponentsTextMeta
+            :aria-label="datePublishedLabel"
+            class="card-poem__meta"
+            :text="datePublished"
+            :title="datePublishedLabel"
+          />
+        </footer>
+      </div>
+    </NuxtLink>
+  </article>
 </template>
 
 <script src="./CardPoem.ts" lang="ts"></script>
