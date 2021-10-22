0ef5314d1a3ddb62be68ae7714af035e01d812a0
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-10-22 15:57:06 +0200

    fix: [#35] jest file transform

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/utils/file.jest.transform.js'
   },
   collectCoverage: true,
   collectCoverageFrom: [
