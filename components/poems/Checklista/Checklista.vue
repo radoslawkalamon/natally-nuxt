064537d56825cc03acf1dae09287858c95047a0d
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-10-25 17:34:10 +0200

    feat: [#53] checklista poem

---
+++ components/poems/Checklista/Checklista.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="checklista"
+    data-test="poems-checklista"
+  >
+    <ComponentsCheckbox
+      checked="checked"
+      class="checklista__checkbox"
+      label="Byle by być"
+      name="byle-by-byc"
+    />
+    <ComponentsCheckbox
+      class="checklista__checkbox"
+      label="Byle by zaliczyć"
+      name="byle-by-zaliczyc"
+    />
+    <ComponentsCheckbox
+      class="checklista__checkbox"
+      label="Punkt po punkcie"
+      name="punkt-po-punkcie"
+    />
+    <ComponentsCheckbox
+      class="checklista__checkbox"
+      label="Życie"
+      name="zycie"
+    />
+    <p class="checklista__last-line">
+      <span class="checklista__gradient">Życie w niebycie</span>
+    </p>
+  </div>
+</template>
+
+<script src="./Checklista.ts" lang="ts"></script>
+<style src="./Checklista.css" scoped></style>
