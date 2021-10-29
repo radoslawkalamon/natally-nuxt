f02f2417c9c4a9edf182a8df105ec60eb3cd8c9e
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-10-29 14:23:39 +0200

    feat: [#48] added scroll event to layout

--- layouts/default.spec.ts
+++ layouts/default.spec.ts
@@ -1,11 +1,16 @@
-import Default from './default.vue'
-import { shallRender } from '@/utils/commonTestSpecs'
+import { createWrapper, mount } from '@vue/test-utils'
+import LayoutDefault from './default.vue'
+import { shallDestroy, shallRender } from '@/utils/commonTestSpecs'
 
 describe('Layouts / Default', () => {
   const defaultOptions = {
+    attachTo: document.body,
     stubs: {
       BlocksHeader: {
         template: '<header data-stub="blocks-header" />'
+      },
+      BlocksDrawer: {
+        template: '<div data-stub="blocks-drawer"><slot /></div>'
       },
       WrappersMain: {
         template: '<div data-stub="wrappers-main"><slot /></div>'
@@ -19,5 +24,21 @@ describe('Layouts / Default', () => {
     }
   }
 
-  shallRender(Default, defaultOptions)
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+
+  shallRender(LayoutDefault, defaultOptions)
+  shallDestroy(LayoutDefault, defaultOptions)
+
+  test('shall emit common/windowScroll on window scroll event', () => {
+    const wrapper = mount(LayoutDefault, defaultOptions)
+
+    window.dispatchEvent(new CustomEvent('scroll'))
+    jest.runAllTimers()
+
+    const rootWrapper = createWrapper(wrapper.vm.$root)
+    const windowScrollCalls = rootWrapper.emitted('common/windowScroll')
+    expect(windowScrollCalls).toBeTruthy()
+  })
 })
--- layouts/default.ts
+++ layouts/default.ts
@@ -1,5 +1,22 @@
 import Vue from 'vue'
+import { throttle } from 'lodash'
 
 export default Vue.extend({
-  name: 'LayoutDefault'
+  name: 'LayoutDefault',
+  beforeMount (): void {
+    this.throttleFunctions()
+
+    window.addEventListener('scroll', this.onWindowScroll)
+  },
+  beforeDestroy (): void {
+    window.removeEventListener('scroll', this.onWindowScroll)
+  },
+  methods: {
+    throttleFunctions (): void {
+      this.onWindowScroll = throttle(this.onWindowScroll, 500)
+    },
+    onWindowScroll (): void {
+      this.$root.$emit('common/windowScroll')
+    }
+  }
 })
