8717c08df217f7f1d8ac8e0928a2467e2645fa50
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-07 11:11:05 +0100

    feat: [#29] list poems wrapper

---
+++ components/wrappers/ListPoems/ListPoems.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="list-poems"
+    data-test="wrappers-list-poems"
+  >
+    <ComponentsLoading v-if="$fetchState.pending" />
+    <slot
+      v-else
+      v-bind="{ metaPostPoems }"
+    />
+  </div>
+</template>
+
+<script src="./ListPoems.ts" lang="ts"></script>
+<style src="./ListPoems.css" scoped></style>
