b8fbc6755bb09caf84af66d76c061db97d908b11
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-07 12:46:00 +0100

    feat: [#31] list stories suggestions block

---
+++ components/blocks/ListStoriesSuggestions/ListStoriesSuggestions.vue
@@ -0,0 +1,40 @@
+<template>
+  <WrappersSection
+    class="list-stories-suggestions"
+    data-test="blocks-list-stories-suggestions"
+  >
+    <template #header>
+      <ComponentsTitle
+        class="list-stories-suggestions__title"
+        :design="2"
+        title="Opowiadania"
+        :type="2"
+      />
+    </template>
+
+    <WrappersListStories
+      class="list-stories-suggestions__stories"
+      :limit="2"
+      :randomize="true"
+    >
+      <template #default="{ metaPostStories }">
+        <template v-for="metaPostStory in metaPostStories">
+          <ComponentsCardStory
+            :key="metaPostStory.path"
+            :meta-post-story="metaPostStory"
+          />
+        </template>
+      </template>
+    </WrappersListStories>
+
+    <template #footer>
+      <ComponentsButtonLink
+        label="Więcej opowiadań"
+        to="/"
+      />
+    </template>
+  </WrappersSection>
+</template>
+
+<script src="./ListStoriesSuggestions.ts" lang="ts"></script>
+<style src="./ListStoriesSuggestions.css" scoped></style>
