50816328639d56721a70b799eda52aa7cf501c86
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-07 15:56:06 +0100

    feat: [#34] list poems suggestions block

---
+++ components/blocks/ListPoemsSuggestions/ListPoemsSuggestions.spec.ts
@@ -0,0 +1,17 @@
+import { merge } from 'lodash'
+import ListPoemsSuggestions from './ListPoemsSuggestions.vue'
+import { shallRender } from '@/devtools/jest.shared.spec'
+
+const defaultOptionsFactory = (options?: object) => merge({
+  stubs: [
+    'WrappersSection',
+    'ComponentsTitle',
+    'WrappersListPoems',
+    'ComponentsCardPoem',
+    'ComponentsButtonLink'
+  ]
+}, options)
+
+describe('Blocks / List Poems Suggestions', () => {
+  shallRender(ListPoemsSuggestions, defaultOptionsFactory())
+})
