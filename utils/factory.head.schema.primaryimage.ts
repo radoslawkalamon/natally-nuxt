96b340dc7183ba260a5657bb58bdabdcd63ac1ba
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-08 16:12:08 +0100

    feat: [#167] factory.head.schema.primaryimage

---
+++ utils/factory.head.schema.primaryimage.ts
@@ -0,0 +1,20 @@
+import { getURL } from './factory.head.utils'
+
+type FactoryHeadSchemaPrimaryimageArgs = {
+  path: string,
+  imageOpenGraph?: string
+}
+
+export const factoryHeadSchemaPrimaryimage = ({
+  path,
+  imageOpenGraph = '/images/common.opengraph.png'
+}: FactoryHeadSchemaPrimaryimageArgs) => ({
+  hid: 'schema:ImageObject',
+  '@type': 'ImageObject',
+  '@id': getURL({ path, anchor: '#primaryimage' }),
+  inLanguage: 'pl-PL',
+  url: getURL({ path: imageOpenGraph }),
+  contentUrl: getURL({ path: imageOpenGraph }),
+  width: 1200,
+  height: 630
+})
