fc6735f7d559b96006df2050bd4355085e2e3a9b
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-09 18:20:50 +0100

    feat: [#167] factory.head snapshots update

--- utils/factory.head.post.poem.spec.ts
+++ utils/factory.head.post.poem.spec.ts
@@ -8,7 +8,7 @@ describe('Utils / Factory / Head / Post / Poem', () => {
       imageCover: '/images/poezja-314/poezja-314.milosc.webp',
       imagePath: '/images/poezja-314/',
       imageOpenGraph: '/images/poezja-314/poezja-314.milosc.opengraph.png',
-      path: '/poezja-314/milosc',
+      path: '/poezja-314/milosc/',
       title: 'Miłość',
       updatedAt: new Date('2021-02-14T12:31:27+00:00')
     }
