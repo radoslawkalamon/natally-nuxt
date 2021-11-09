9c3615e8957da1f7443122c0dcf8e380153a0d2f
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-09 18:56:44 +0100

    feat: [#45] audiobooki page

---
+++ pages/audiobooki.vue
@@ -0,0 +1,13 @@
+<template>
+  <article class="page-audiobooki">
+    <ComponentsPageMeta
+      :title="dtoMetaPage.title"
+    />
+    <WrappersText>
+      <NuxtContent :document="page" />
+    </WrappersText>
+    <BlocksListAudiobookServices />
+  </article>
+</template>
+
+<script src="./audiobooki.ts" lang="ts"></script>
