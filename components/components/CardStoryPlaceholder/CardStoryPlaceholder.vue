dcadc05371aec794ceee358648ffbf096193fb02
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-12 15:32:04 +0100

    feat: [#196] card story placeholder component

---
+++ components/components/CardStoryPlaceholder/CardStoryPlaceholder.vue
@@ -0,0 +1,9 @@
+<template>
+  <span
+    class="card-story-placeholder"
+    data-test="components-card-story-placeholder"
+  />
+</template>
+
+<script src="./CardStoryPlaceholder.ts" lang="ts"></script>
+<style src="./CardStoryPlaceholder.css" scoped></style>
