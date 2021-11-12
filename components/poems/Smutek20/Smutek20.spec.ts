2ff8c411f22f00bc87530fe5546efe4b89369efd
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-12 11:51:01 +0100

    feat: [#203] smutek 2.0 poem component

---
+++ components/poems/Smutek20/Smutek20.spec.ts
@@ -0,0 +1,6 @@
+import Smutek20 from './Smutek20.vue'
+import { shallRender } from '@/devtools/jest.shared.spec'
+
+describe('Poems / Smutek 2.0', () => {
+  shallRender(Smutek20)
+})
---
+++ components/poems/Smutek20/Smutek20.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="smutek-2-0"
+    data-test="poems-smutek-2-0"
+  >
+    <blockquote
+      class="twitter-tweet"
+      data-lang="pl"
+    >
+      <p
+        lang="und"
+        dir="ltr"
+      >
+        :(
+      </p>&mdash; Radosław Kalamon (@radoslawkalamon) <a href="https://twitter.com/radoslawkalamon/status/833004185696206852?ref_src=twsrc%5Etfw">18 lutego 2017</a>
+    </blockquote>
+    <script
+      async
+      src="https://platform.twitter.com/widgets.js"
+      charset="utf-8"
+    />
+  </div>
+</template>
+
+<script src="./Smutek20.ts" lang="ts"></script>
+<style src="./Smutek20.css"></style>
