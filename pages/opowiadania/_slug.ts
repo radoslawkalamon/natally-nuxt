aa1b6e6f39a559e875691176e88f3a86792b3f36
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-13 22:14:20 +0100

    feat: [#61] stories page

---
+++ pages/opowiadania/_slug.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import type { MetaInfo } from 'vue-meta'
+import { DTOMetaPostStory } from '@/utils/dto.meta.post.story'
+import { factoryHeadPostStory } from '@/utils/factory.head.post.story'
+import { formatterDateMeta } from '@/utils/formatter.date.meta'
+
+export default Vue.extend({
+  async asyncData ({ $content, params, error }) {
+    const article = await $content('opowiadania', params.slug)
+      .fetch()
+
+    if (!article) {
+      return error({ statusCode: 404, message: 'Article not found' })
+    }
+
+    return { article }
+  },
+  head (): MetaInfo {
+    return factoryHeadPostStory(this.dtoMetaPostStory)
+  },
+  computed: {
+    dtoMetaPostStory (): DTOMetaPostStory {
+      // @ts-ignore: Remove ts-ignore for Nuxt 2.16.0
+      return new DTOMetaPostStory(this.article)
+    },
+    storyDatePublished (): string {
+      return formatterDateMeta(this.dtoMetaPostStory.createdAt)
+    }
+  }
+})
---
+++ pages/opowiadania/_slug.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="post-stories">
+    <ComponentsCoverStory
+      :image="dtoMetaPostStory.imageCover"
+      :image2x="dtoMetaPostStory.imageCover2x"
+    />
+    <ComponentsPageMeta
+      :date-published="storyDatePublished"
+      :title="dtoMetaPostStory.title"
+    />
+    <!-- <BlocksAudiobook :id="dtoMetaPostStory.audiobookId" /> -->
+    <WrappersTextStories>
+      <NuxtContent
+        data-progress-bar-hook
+        :document="article"
+      />
+    </WrappersTextStories>
+    <BlocksAdjacentPostLinksStory
+      :slug="article.slug"
+    />
+    <BlocksListStoriesSuggestions />
+    <BlocksListPoemsSuggestions />
+    <BlocksProgressBar />
+  </article>
+</template>
+
+<script src="./_slug.ts" lang="ts"></script>
