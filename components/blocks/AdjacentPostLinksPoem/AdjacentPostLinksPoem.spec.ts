d02edbf73a1c0f015340c281a1069df829fe37f4
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-13 18:22:03 +0100

    test: [#134] adjacent post links poem block

--- components/blocks/AdjacentPostLinksPoem/AdjacentPostLinksPoem.spec.ts
+++ components/blocks/AdjacentPostLinksPoem/AdjacentPostLinksPoem.spec.ts
@@ -1,6 +1,45 @@
+import { merge } from 'lodash'
+import { mount } from '@vue/test-utils'
 import AdjacentPostLinksPoem from './AdjacentPostLinksPoem.vue'
-import { shallRender } from '@/devtools/jest.shared.spec'
+import { MockNuxtContent } from '@/devtools/jest.mock.nuxt.content'
+
+jest.mock('@/utils/dto.meta.post.poem', () => ({
+  DTOMetaPostPoem: class MockEmptyClass {}
+}))
+
+const defaultOptionsFactory = (options?: object) => merge({
+  mocks: {
+    $fetchState: {
+      pending: false
+    },
+    $content: MockNuxtContent(Array(2).fill({}))
+  },
+  propsData: {
+    slug: 'test-slug'
+  },
+  stubs: [
+    'ComponentsAdjacentPostLinkPlaceholder',
+    'ComponentsAdjacentPostLinkPrevious',
+    'ComponentsAdjacentPostLinkNext'
+  ]
+}, options)
 
 describe('Blocks / Adjacent Post Links Poem', () => {
-  shallRender(AdjacentPostLinksPoem)
+  test('shall render', async (): Promise<void> => {
+    const wrapper = mount(AdjacentPostLinksPoem, defaultOptionsFactory())
+    await (AdjacentPostLinksPoem as any).options.fetch.call(wrapper.vm)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('shall show placeholders when loading', async (): Promise<void> => {
+    const wrapper = mount(AdjacentPostLinksPoem, defaultOptionsFactory({
+      mocks: {
+        $fetchState: {
+          pending: true
+        }
+      }
+    }))
+    await (AdjacentPostLinksPoem as any).options.fetch.call(wrapper.vm)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
 })
