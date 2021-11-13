4dd5a33385cb0bd794211e82e6e34aa820b68a6e
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-13 21:54:20 +0100

    feat: [#130] default layout resize event

--- layouts/default.spec.ts
+++ layouts/default.spec.ts
@@ -1,3 +1,4 @@
+import { merge } from 'lodash'
 import { createLocalVue, createWrapper, mount } from '@vue/test-utils'
 import Vuex, { Store } from 'vuex'
 import LayoutDefault from './default.vue'
@@ -12,30 +13,30 @@ const store = new Store({
   }
 })
 
-describe('Layouts / Default', () => {
-  const defaultOptions = {
-    attachTo: document.body,
-    localVue,
-    store,
-    stubs: [
-      'BlocksHeader',
-      'BlocksDrawer',
-      'WrappersMain',
-      'Nuxt',
-      'BlocksFooter',
-      'BlocksModalPrivacy'
-    ]
-  }
+const defaultOptionsFactory = (options?: object) => merge({
+  attachTo: document.body,
+  localVue,
+  store,
+  stubs: [
+    'BlocksHeader',
+    'BlocksDrawer',
+    'WrappersMain',
+    'Nuxt',
+    'BlocksFooter',
+    'BlocksModalPrivacy'
+  ]
+}, options)
 
+describe('Layouts / Default', () => {
   beforeAll(() => {
     jest.useFakeTimers()
   })
 
-  shallRender(LayoutDefault, defaultOptions)
-  shallDestroy(LayoutDefault, defaultOptions)
+  shallRender(LayoutDefault, defaultOptionsFactory())
+  shallDestroy(LayoutDefault, defaultOptionsFactory())
 
   test('shall emit common/windowScroll on window scroll event', () => {
-    const wrapper = mount(LayoutDefault, defaultOptions)
+    const wrapper = mount(LayoutDefault, defaultOptionsFactory())
 
     window.dispatchEvent(new CustomEvent('scroll'))
     jest.runAllTimers()
@@ -43,5 +44,16 @@ describe('Layouts / Default', () => {
     const rootWrapper = createWrapper(wrapper.vm.$root)
     const windowScrollCalls = rootWrapper.emitted('common/windowScroll')
     expect(windowScrollCalls).toBeTruthy()
+  })
+
+  test('shall emit common/windowResize on window resize event', () => {
+    const wrapper = mount(LayoutDefault, defaultOptionsFactory())
+
+    window.dispatchEvent(new CustomEvent('resize'))
+    jest.runAllTimers()
+
+    const rootWrapper = createWrapper(wrapper.vm.$root)
+    const windowResizeCalls = rootWrapper.emitted('common/windowResize')
+    expect(windowResizeCalls).toBeTruthy()
   })
 })
