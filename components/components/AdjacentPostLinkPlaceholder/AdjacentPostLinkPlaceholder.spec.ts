7a03ca576ddedb67b1ad1bf1b6dc30bff33b1f26
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-13 18:11:18 +0100

    feat: [#134] adjacent post link placeholder

---
+++ components/components/AdjacentPostLinkPlaceholder/AdjacentPostLinkPlaceholder.spec.ts
@@ -0,0 +1,6 @@
+import AdjacentPostLinkPlaceholder from './AdjacentPostLinkPlaceholder.vue'
+import { shallRender } from '@/devtools/jest.shared.spec'
+
+describe('Components / Adjacent Post Link Placeholder', () => {
+  shallRender(AdjacentPostLinkPlaceholder)
+})
