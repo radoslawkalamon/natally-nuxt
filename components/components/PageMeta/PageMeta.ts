630532057f6cc7c2aa21177f4d76d5e134ac32f0
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-16 15:28:44 +0100

    feat: [#250] datePublished to createdAt change

--- components/components/PageMeta/PageMeta.ts
+++ components/components/PageMeta/PageMeta.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue'
+import { formatterDateMeta } from '@/utils/formatter.date.meta'
 
 export default Vue.extend({
   name: 'ComponentsPageMeta',
   props: {
-    datePublished: {
-      type: String,
-      default: ''
+    createdAt: {
+      type: Date,
+      default: () => new Date(0)
     },
     title: {
       type: String,
@@ -13,8 +14,14 @@ export default Vue.extend({
     }
   },
   computed: {
-    datePublishedLabel (): string {
-      return `Opublikowano ${this.datePublished}`
+    dateMeta (): string {
+      return formatterDateMeta(this.createdAt)
+    },
+    dateMetaLabel (): string {
+      return `Opublikowano ${this.dateMeta}`
+    },
+    shallShowDate (): boolean {
+      return this.createdAt.valueOf() !== 0
     }
   }
 })
