ef0e9d92ffb22fe4fe5b281dd83dabf12c816f7c
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-18 11:11:05 +0100

    chore: [#263] remove bad alias from configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   plugins: [],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
-    { path: '~/components', extensions: ['vue'] }
+    { path: '@/components', extensions: ['vue'] }
   ],
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
