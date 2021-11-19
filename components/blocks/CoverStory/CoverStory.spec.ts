aa34dab029e240ec9b7440e684ae3d4593638953
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-19 14:10:31 +0100

    fix: minor fixes

--- components/blocks/CoverStory/CoverStory.spec.ts
+++ components/blocks/CoverStory/CoverStory.spec.ts
@@ -4,8 +4,10 @@ import { shallRender } from '@/devtools/jest.shared.spec'
 describe('Blocks / Cover Story', () => {
   shallRender(CoverStory, {
     propsData: {
-      image: 'https://www.fillmurray.com/600/300',
-      image2x: 'https://www.fillmurray.com/1200/600'
+      images: [
+        'https://www.fillmurray.com/600/300',
+        'https://www.fillmurray.com/1200/600'
+      ]
     },
     stubs: ['ComponentsCover']
   })
--- components/blocks/ListPoemsMain/ListPoemsMain.spec.ts
+++ components/blocks/ListPoemsMain/ListPoemsMain.spec.ts
@@ -2,10 +2,6 @@ import merge from 'lodash/merge'
 import ListPoemsMain from './ListPoemsMain.vue'
 import { shallRender } from '@/devtools/jest.shared.spec'
 
-describe('Blocks / List Poems Main', () => {
-  shallRender(ListPoemsMain)
-})
-
 const defaultOptionsFactory = (options?: object) => merge({
   stubs: [
     'WrappersListPoems',
