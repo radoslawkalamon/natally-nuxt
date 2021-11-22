55c81f4cde924e8b6b3d60cdd0e9dfc4e429c8f7
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-22 12:26:49 +0100

    feat: [#197] lazy hydration package installed

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,32 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module 'vue-lazy-hydration' {
+  import type { Component, AsyncComponent } from 'vue'
+
+  const LazyHydrate: Component<
+    { hydrated: boolean },
+    {
+      cleanup(): void
+      hydrate(): void
+    },
+    {
+      interactionEvents: string[]
+    },
+    {
+      idleTimeout: number
+      onInteraction: boolean | string | string[]
+      ssrOnly: boolean
+      triggerHydration: boolean
+      whenIdle: boolean
+    }
+  > & { functional: false }
+
+  export function hydrateNever(component: AsyncComponent | Component): AsyncComponent | Component
+  export function hydrateWhenIdle(component: AsyncComponent | Component, options?: {ignoredProps?: string[]}): AsyncComponent | Component
+  export function hydrateWhenVisible(component: AsyncComponent | Component, options?: {ignoredProps?: string[], observerOptions?: string[]}): AsyncComponent | Component
+  export function hydrateOnInteraction(component: AsyncComponent | Component, options?: { event?: string, ignoredProps?: string[] }): AsyncComponent | Component
+
+  export default LazyHydrate
+}
