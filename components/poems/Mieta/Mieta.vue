1f41911d5c3ee66c2b2b5d4541700f87783b58bb
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-22 15:34:56 +0100

    feat: [#281] mieta poem

---
+++ components/poems/Mieta/Mieta.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="mieta"
+    data-test="poems-mieta"
+  >
+    <p class="mieta__text-align-center">
+      Najpierw była
+    </p>
+    <p>&nbsp;</p>
+    <div class="mieta__text-clash">
+      <p>między</p>
+      <p>nami</p>
+      <p><i>mięta</i></p>
+    </div>
+    <p>&nbsp;</p>
+    <p class="mieta__text-align-right">
+      A teraz jestem
+    </p>
+    <p class="mieta__text-align-right">
+      <i>pomięta.</i>
+    </p>
+  </div>
+</template>
+
+<script src="./Mieta.ts" lang="ts"></script>
+<style src="./Mieta.css" scoped></style>
