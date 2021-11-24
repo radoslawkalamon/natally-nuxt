a05c152e0b340b605207c5d15f20c44e0f93a943
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-24 18:18:16 +0100

    feat: [#57] promocja lgbt poem

---
+++ components/poems/PromocjaLGBT/PromocjaLGBT.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="promocja-lgbt"
+    data-test="poems-promocja-lgbt"
+  >
+    <div
+      class="lgbt-product-card"
+      @click="buyClick"
+    >
+      <transition-group
+        class="lgbt-product-card__image-wrapper"
+        name="transition-lgbt-image-wrapper"
+        tag="div"
+      >
+        <template v-for="(image, index) in images">
+          <img
+            v-if="index === activeImage"
+            :key="index"
+            class="lgbt-product-card__image"
+            v-bind="image"
+          >
+        </template>
+      </transition-group>
+      <div class="lgbt-product-card__content-wrapper">
+        <div class="lgbt-product-card__ribbon">
+          <img
+            alt="Produkt w promocji!"
+            class="lgbt-product-card__ribbon-icon"
+            src="./images/ribbon.svg"
+            title="Produkt w promocji!"
+          >
+        </div>
+        <h2 class="lgbt-product-card__title">Miłość</h2>
+        <span class="lgbt-product-card__price">0,00 zł</span>
+        <button
+          aria-label="Dodaj do koszyka"
+          class="lgbt-product-card__add-to-cart"
+          title="Dodaj do koszyka"
+          type="button"
+        >
+          <img
+            class="lgbt-product-card__add-to-cart-icon"
+            src="./images/magnifier.svg"
+          >
+          Sprawdź!
+        </button>
+      </div>
+      <div
+        v-if="startAnimation"
+        class="lgbt-reaction-wrapper"
+      >
+        <template v-for="i in heartCount">
+          <img
+            :key="i"
+            alt="Serce"
+            class="lgbt-reaction-wrapper__heart"
+            src="./images/heart.svg"
+            :style="heartStyles[i]"
+            title="Serce"
+          >
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./PromocjaLGBT.ts" lang="ts"></script>
+<style src="./PromocjaLGBT.css"></style>
