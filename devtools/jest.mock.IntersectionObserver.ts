e5bfe485f79a899d67cdf34e5126b68e549d6986
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-24 13:51:12 +0100

    test: [#282] intersection observer mock

---
+++ devtools/jest.mock.IntersectionObserver.ts
@@ -0,0 +1,10 @@
+export const jestMockIntersectionObserver = () => {
+  Object.defineProperty(window, 'IntersectionObserver', {
+    writable: true,
+    value: jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn()
+    }))
+  })
+}
