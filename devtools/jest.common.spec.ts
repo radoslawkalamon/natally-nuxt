f3b490d1fc18251491b9c6fca0caf35ae9e134f8
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-26 00:35:14 +0100

    feat: [#303] basic specs

---
+++ devtools/jest.common.spec.ts
@@ -0,0 +1,40 @@
+import {
+  createComponentFromMixin,
+  createIntegrationTestWrapper,
+  createUnitTestWrapper
+} from '@/devtools/jest.common.spec.utils'
+
+export const expectHeadMatchSnapshot = ({ wrapper }: {
+  wrapper: Awaited<ReturnType<typeof createUnitTestWrapper>> | Awaited<ReturnType<typeof createIntegrationTestWrapper>>
+}) => {
+  if (wrapper.vm.$options.head) {
+    const head = typeof wrapper.vm.$options.head === 'function'
+      ? wrapper.vm.$options.head()
+      : wrapper.vm.$options.head
+    expect(head).toMatchSnapshot()
+  }
+}
+
+export const shallPassIntegrationSanityTest = ({ component, options }: {} & Parameters<typeof createIntegrationTestWrapper>[0]) => {
+  test('shall pass integration sanity test', async () => {
+    const wrapper = await createIntegrationTestWrapper({ component, options })
+    expect(wrapper.html()).toMatchSnapshot()
+    wrapper.destroy()
+  })
+}
+
+export const shallPassUnitSanityTest = ({ component, options }: {} & Parameters<typeof createUnitTestWrapper>[0]) => {
+  test('shall pass unit sanity test', async () => {
+    const wrapper = await createUnitTestWrapper({ component, options })
+    expect(wrapper.html()).toMatchSnapshot()
+    wrapper.destroy()
+  })
+}
+
+export const shallPassMixinSanityTest = ({ mixin, options }: {} & Parameters<typeof createComponentFromMixin>[0]) => {
+  test('shall pass mixin sanity test', async () => {
+    const component = createComponentFromMixin({ mixin, options })
+    const wrapper = await createUnitTestWrapper({ component })
+    wrapper.destroy()
+  })
+}
