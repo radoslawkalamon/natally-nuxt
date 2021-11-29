a1856f3f14a069978d6235ac9495141ee4c706d4
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-29 16:48:11 +0100

    test: [#306] cover component

--- components/components/Cover/Cover.spec.ts
+++ components/components/Cover/Cover.spec.ts
@@ -1,38 +1,43 @@
-import { mount } from '@vue/test-utils'
 import Cover from '@/components/components/Cover/Cover.vue'
-import { shallRender } from '@/devtools/jest.shared.spec'
+import { shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
+import { createDefaultOptionsFactory, createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
+
+const defaultOptionsFactory = createDefaultOptionsFactory({
+  propsData: {
+    images: ['/images/post.test-post.1x.jpg']
+  }
+})
 
 describe('Components / Cover', () => {
-  shallRender(Cover, {
-    propsData: {
-      images: [
-        '/images/post.test-post.1x.jpg'
-      ]
-    }
-  })
-
-  test('shall vm.style output image-set with 1 image', () => {
-    const wrapper = mount(Cover, {
-      propsData: {
-        images: [
-          '/images/post.test-post.1x.jpg'
-        ]
-      }
+  describe('Unit', () => {
+    shallPassUnitSanityTest({
+      component: Cover,
+      options: defaultOptionsFactory()
     })
-    // @ts-ignore
-    expect(wrapper.vm.style).toMatchSnapshot()
-  })
 
-  test('shall vm.style output image-set with 2 image', () => {
-    const wrapper = mount(Cover, {
-      propsData: {
-        images: [
-          '/images/post.test-post.1x.jpg',
-          '/images/post.test-post.2x.jpg'
-        ]
-      }
+    test('shall vm.style output image-set with 1 image', async () => {
+      const wrapper = await createUnitTestWrapper({
+        component: Cover,
+        options: defaultOptionsFactory()
+      })
+      // @ts-ignore
+      expect(wrapper.vm.style).toMatchSnapshot()
     })
-    // @ts-ignore
-    expect(wrapper.vm.style).toMatchSnapshot()
+
+    test('shall vm.style output image-set with 2 image', async () => {
+      const wrapper = await createUnitTestWrapper({
+        component: Cover,
+        options: defaultOptionsFactory({
+          propsData: {
+            images: [
+              '/images/post.test-post.1x.jpg',
+              '/images/post.test-post.2x.jpg'
+            ]
+          }
+        })
+      })
+      // @ts-ignore
+      expect(wrapper.vm.style).toMatchSnapshot()
+    })
   })
 })
