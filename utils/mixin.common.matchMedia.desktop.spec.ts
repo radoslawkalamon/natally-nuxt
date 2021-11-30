9b36bf5241526982988f5ded8100bd2472f37604
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-30 21:46:11 +0100

    test: [#306] common matchMedia desktop mixin

--- utils/mixin.common.matchMedia.desktop.spec.ts
+++ utils/mixin.common.matchMedia.desktop.spec.ts
@@ -1,35 +1,30 @@
-import Vue from 'vue'
-import type { VueConstructor } from 'vue'
-import { mount } from '@vue/test-utils'
+import { shallPassMixinSanityTest } from '@/devtools/jest.common.spec'
+import { createComponentFromMixin, createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
 import { JestMockMatchMedia } from '@/devtools/jest.mock.matchMedia'
 import mixinCommonMatchMediaDesktop from '@/utils/mixin.common.matchMedia.desktop'
 
-const Component = (Vue as VueConstructor<
-  Vue
-  & InstanceType<typeof mixinCommonMatchMediaDesktop>
->).extend({
-  mixins: [mixinCommonMatchMediaDesktop],
-  template: '<div />'
+JestMockMatchMedia()
+
+const mixinComponent = createComponentFromMixin({
+  mixin: mixinCommonMatchMediaDesktop
 })
 
 describe('Utils / Mixins / MatchMedia / Desktop', () => {
-  beforeAll(() => {
-    JestMockMatchMedia()
+  shallPassMixinSanityTest({
+    mixin: mixinCommonMatchMediaDesktop
   })
 
-  test('shall mount & destroy', () => {
-    expect(() => {
-      const wrapper = mount(Component)
-      wrapper.destroy()
-    }).not.toThrowError()
-  })
-
-  test('shall update common/matchMedia/desktop/matches when common/matchMedia/desktop/onChange called', () => {
+  test('shall update common/matchMedia/desktop/matches when common/matchMedia/desktop/onChange called', async () => {
     const e: Pick<MediaQueryListEvent, 'matches'> = {
       matches: true
     }
-    const wrapper = mount(Component)
+
+    const wrapper = await createUnitTestWrapper({ component: mixinComponent })
+    // @ts-ignore: no mixinComponent typing?
     wrapper.vm['common/matchMedia/desktop/onChange'](e as MediaQueryListEvent)
+    // @ts-ignore: no mixinComponent typing?
     expect(wrapper.vm['common/matchMedia/desktop/matches']).toBeTruthy()
+
+    wrapper.destroy()
   })
 })
