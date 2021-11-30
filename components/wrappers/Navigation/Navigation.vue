a0c07e188af36cd56e63b57f19490b1987c3969a
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-30 20:14:31 +0100

    refactor: [#306] navigation wrapper

--- components/wrappers/Navigation/Navigation.vue
+++ components/wrappers/Navigation/Navigation.vue
@@ -1,2 +1,18 @@
+<template>
+  <ul
+    v-if="children.length !== 0"
+    class="navigation"
+  >
+    <template v-for="(child, index) in children">
+      <li
+        :key="index"
+        class="navigation__item"
+      >
+        <vnode :node="child" />
+      </li>
+    </template>
+  </ul>
+</template>
+
 <script src="./Navigation.ts" lang="ts"></script>
 <style src="./Navigation.css" scoped></style>
