145a6dd63154f0a3d5b07dd072eb28bfe0601785
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-30 20:22:53 +0100

    test: [#306] section wrapper

--- components/wrappers/Section/Section.spec.ts
+++ components/wrappers/Section/Section.spec.ts
@@ -1,11 +1,33 @@
-import Section from './Section.vue'
-import { shallHaveSlot, shallHideSlotWhenNoChildren, shallRender } from '@/devtools/jest.shared.spec'
+import Section from '@/components/wrappers/Section/Section.vue'
+import { shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
+import { createDefaultOptionsFactory } from '@/devtools/jest.common.spec.utils'
+
+const defaultOptionsFactory = createDefaultOptionsFactory({
+  slots: {
+    default: 'This is default slot test'
+  }
+})
 
 describe('Wrappers / Section', () => {
-  shallRender(Section)
-  shallHaveSlot(Section, 'header')
-  shallHideSlotWhenNoChildren(Section, 'header', 'wrappers-section-header')
-  shallHaveSlot(Section, 'default')
-  shallHaveSlot(Section, 'footer')
-  shallHideSlotWhenNoChildren(Section, 'footer', 'wrappers-section-footer')
+  describe('Unit', () => {
+    shallPassUnitSanityTest({
+      component: Section,
+      description: 'no footer slot',
+      options: defaultOptionsFactory({
+        slots: {
+          header: 'This is header slot test'
+        }
+      })
+    })
+
+    shallPassUnitSanityTest({
+      component: Section,
+      description: 'no header slot',
+      options: defaultOptionsFactory({
+        slots: {
+          footer: 'This is footer slot test'
+        }
+      })
+    })
+  })
 })
