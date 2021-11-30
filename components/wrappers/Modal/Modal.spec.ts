556967df51b7f606f3735773191e826257bbf2c5
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-30 20:18:28 +0100

    test: [#306] modal wrapper

--- components/wrappers/Modal/Modal.spec.ts
+++ components/wrappers/Modal/Modal.spec.ts
@@ -1,18 +1,30 @@
-import Modal from './Modal.vue'
-import { shallRender, shallHaveSlot } from '@/devtools/jest.shared.spec'
+import Modal from '@/components/wrappers/Modal/Modal.vue'
+import { createDefaultOptionsFactory } from '@/devtools/jest.common.spec.utils'
+import { shallPassIntegrationSanityTest, shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
+
+const defaultOptionsFactory = createDefaultOptionsFactory({
+  propsData: {
+    title: 'Test title'
+  },
+  slots: {
+    default: 'This is default slot test'
+  }
+})
 
 describe('Wrappers / Modal', () => {
-  const defaultOptions = {
-    propsData: {
-      title: 'Test title'
-    },
-    stubs: {
-      ComponentsTitle: {
-        template: '<h1 data-stub="components-title" />'
-      }
-    }
-  }
+  describe('Unit', () => {
+    shallPassUnitSanityTest({
+      component: Modal,
+      options: defaultOptionsFactory({
+        stubs: ['ComponentsTitle']
+      })
+    })
+  })
 
-  shallRender(Modal, defaultOptions)
-  shallHaveSlot(Modal, 'default', defaultOptions)
+  describe('Integration', () => {
+    shallPassIntegrationSanityTest({
+      component: Modal,
+      options: defaultOptionsFactory()
+    })
+  })
 })
