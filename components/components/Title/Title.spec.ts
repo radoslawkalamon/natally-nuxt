3721af5fc225e03f6989b025c4824498e24d1962
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-30 12:13:26 +0100

    test: [#306] title component

--- components/components/Title/Title.spec.ts
+++ components/components/Title/Title.spec.ts
@@ -1,43 +1,39 @@
-import { mount } from '@vue/test-utils'
-import Title from './Title.vue'
-import { shallRender, shallHaveStringProp, shallHavePropInfluenceOverClassNames } from '@/devtools/jest.shared.spec'
+import Title from '@/components/components/Title/Title.vue'
+import { createDefaultOptionsFactory } from '@/devtools/jest.common.spec.utils'
+import { shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
 
-describe('Components/Logo', () => {
-  const defaultOptions = {
-    propsData: {
-      title: 'Default Prop Title'
-    }
+const defaultOptionsFactory = createDefaultOptionsFactory({
+  propsData: {
+    title: 'Default title'
   }
+})
 
-  shallRender(Title, defaultOptions)
+describe('Components / Title', () => {
+  shallPassUnitSanityTest({
+    component: Title,
+    description: 'defaults',
+    options: defaultOptionsFactory()
+  })
 
-  describe('Props', () => {
-    shallHaveStringProp(Title, 'title', defaultOptions)
-
-    test("shall have number prop 'type' influence over wrapper tag", () => {
-      const type = 3
-      const wrapper = mount(Title, {
-        propsData: {
-          ...defaultOptions.propsData,
-          type
-        }
-      })
-      expect(wrapper.element.tagName.toLowerCase()).toBe('h3')
+  shallPassUnitSanityTest({
+    component: Title,
+    description: 'with optional props passed',
+    options: defaultOptionsFactory({
+      propsData: {
+        type: 3,
+        design: 1,
+        shallShowUnderscore: false
+      }
     })
+  })
 
-    test("shall have number prop 'type' no influence over wrapper classes", () => {
-      const type = 3
-      const wrapper = mount(Title, {
-        propsData: {
-          ...defaultOptions.propsData,
-          type
-        }
-      })
-      const isWrapperHaveClass = wrapper.classes().some(cls => cls.includes('design-3'))
-      expect(isWrapperHaveClass).toBeFalsy()
+  shallPassUnitSanityTest({
+    component: Title,
+    description: 'type have no influence over design',
+    options: defaultOptionsFactory({
+      propsData: {
+        type: 4
+      }
     })
-
-    shallHavePropInfluenceOverClassNames(Title, 'design', 4, 'design-4', defaultOptions)
-    shallHavePropInfluenceOverClassNames(Title, 'shallShowUnderscore', true, 'underscore', defaultOptions)
   })
 })
