a206b1ce5b426bee70c6fbabc6226d10a2811e06
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-11-30 20:58:29 +0100

    test: [#306] common windowScroll values mixin

--- utils/mixin.common.windowScroll.values.spec.ts
+++ utils/mixin.common.windowScroll.values.spec.ts
@@ -1,32 +1,28 @@
-import Vue from 'vue'
-import type { VueConstructor } from 'vue'
-import { mount } from '@vue/test-utils'
+import { createComponentFromMixin, createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
 import mixinCommonWindowScrollValues from '@/utils/mixin.common.windowScroll.values'
-
-const Component = (Vue as VueConstructor<
-  Vue
-  & InstanceType<typeof mixinCommonWindowScrollValues>
->).extend({
-  mixins: [mixinCommonWindowScrollValues],
-  template: '<div />'
-})
+import { shallPassMixinSanityTest } from '@/devtools/jest.common.spec'
 
 jest.mock('lodash/throttle', () => (cb: Function) => cb)
 
-describe('Utils / Mixins / windowScroll / Emitter', () => {
-  test('shall mount & destroy', () => {
-    expect(() => {
-      const wrapper = mount(Component)
-      wrapper.destroy()
-    }).not.toThrowError()
+const mixinComponent = createComponentFromMixin({
+  mixin: mixinCommonWindowScrollValues
+})
+
+describe('Utils / Mixins / windowScroll / Values', () => {
+  shallPassMixinSanityTest({
+    mixin: mixinCommonWindowScrollValues
   })
 
-  test('shall update data on common/windowScroll', () => {
-    const wrapper = mount(Component)
+  test('shall update data on common/windowScroll', async () => {
+    const wrapper = await createUnitTestWrapper({
+      component: mixinComponent
+    })
     // @ts-ignore: window.scrollY mock
     window.scrollY = 500
     wrapper.vm.$root.$emit('common/windowScroll')
+    // @ts-ignore: no mixinComponent typing?
     expect(wrapper.vm['common/windowScroll/scrollPosition']).toBeGreaterThan(0)
+    // @ts-ignore: no mixinComponent typing?
     expect(wrapper.vm['common/windowScroll/scrollDelta']).toBeGreaterThan(0)
   })
 })
