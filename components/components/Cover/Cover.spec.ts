6d8a8348f1456ed893147e01a208b3902dab235c
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-02 14:29:56 +0100

    test: [#306] cover added missing test

--- components/components/Cover/Cover.spec.ts
+++ components/components/Cover/Cover.spec.ts
@@ -1,24 +1,30 @@
 import Cover from '@/components/components/Cover/Cover.vue'
 import { shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
-import { createDefaultOptionsFactory, createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
-
-const defaultOptionsFactory = createDefaultOptionsFactory({
-  propsData: {
-    images: ['/images/post.test-post.1x.jpg']
-  }
-})
+import { createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
 
 describe('Components / Cover', () => {
   describe('Unit', () => {
     shallPassUnitSanityTest({
-      component: Cover,
-      options: defaultOptionsFactory()
+      component: Cover
+    })
+
+    test('shall vm.style output empty object with 0 image', async () => {
+      const wrapper = await createUnitTestWrapper({
+        component: Cover
+      })
+      // @ts-ignore
+      expect(wrapper.vm.style).toMatchSnapshot()
+      wrapper.destroy()
     })
 
     test('shall vm.style output image-set with 1 image', async () => {
       const wrapper = await createUnitTestWrapper({
         component: Cover,
-        options: defaultOptionsFactory()
+        options: {
+          propsData: {
+            images: ['/images/post.test-post.1x.jpg']
+          }
+        }
       })
       // @ts-ignore
       expect(wrapper.vm.style).toMatchSnapshot()
@@ -28,14 +34,14 @@ describe('Components / Cover', () => {
     test('shall vm.style output image-set with 2 image', async () => {
       const wrapper = await createUnitTestWrapper({
         component: Cover,
-        options: defaultOptionsFactory({
+        options: {
           propsData: {
             images: [
               '/images/post.test-post.1x.jpg',
               '/images/post.test-post.2x.jpg'
             ]
           }
-        })
+        }
       })
       // @ts-ignore
       expect(wrapper.vm.style).toMatchSnapshot()
