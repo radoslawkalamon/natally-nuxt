c1d224c0db014b1dfa795e7da36502676c25f552
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-02 16:49:05 +0100

    test: [#306] cover poem block

--- components/blocks/CoverPoem/CoverPoem.spec.ts
+++ components/blocks/CoverPoem/CoverPoem.spec.ts
@@ -1,13 +1,50 @@
-import CoverPoem from './CoverPoem.vue'
-import { shallRender } from '@/devtools/jest.shared.spec'
+import CoverPoem from '@/components/blocks/CoverPoem/CoverPoem.vue'
+import { shallPassIntegrationSanityTest, shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
 
 describe('Blocks / Cover Poem', () => {
-  shallRender(CoverPoem, {
-    propsData: {
-      images: [
-        '/images/post.test-post.1x.jpg'
-      ]
-    },
-    stubs: ['ComponentsCover']
+  describe('Unit', () => {
+    shallPassUnitSanityTest({
+      component: CoverPoem,
+      description: 'no images',
+      options: {
+        propsData: {
+          images: []
+        },
+        stubs: ['ComponentsCover']
+      }
+    })
+
+    shallPassUnitSanityTest({
+      component: CoverPoem,
+      description: 'with images',
+      options: {
+        propsData: {
+          images: ['/images/post.test-post.1x.jpg']
+        },
+        stubs: ['ComponentsCover']
+      }
+    })
+  })
+
+  describe('Integration', () => {
+    shallPassIntegrationSanityTest({
+      component: CoverPoem,
+      description: 'no images',
+      options: {
+        propsData: {
+          images: []
+        }
+      }
+    })
+
+    shallPassIntegrationSanityTest({
+      component: CoverPoem,
+      description: 'with images',
+      options: {
+        propsData: {
+          images: ['/images/post.test-post.1x.jpg']
+        }
+      }
+    })
   })
 })
