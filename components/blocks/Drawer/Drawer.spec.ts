0db1c9a756acbe0a8fd212f0151cf0919d2b78cd
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-02 17:18:44 +0100

    test: [#306] drawer block

--- components/blocks/Drawer/Drawer.spec.ts
+++ components/blocks/Drawer/Drawer.spec.ts
@@ -1,19 +1,91 @@
-import merge from 'lodash/merge'
-import Drawer from './Drawer.vue'
-import { shallRender } from '@/devtools/jest.shared.spec'
+import Drawer from '@/components/blocks/Drawer/Drawer.vue'
+import { shallPassIntegrationSanityTest, shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
+import { createDefaultOptionsFactory, createIntegrationTestWrapper, createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
 import { JestMockMatchMedia } from '@/devtools/jest.mock.matchMedia'
 
-const defaultOptionsFactory = (options?: object) => merge({
+JestMockMatchMedia()
+
+const defaultOptionsUnitFactory = createDefaultOptionsFactory({
   stubs: [
     'ComponentsNavigationDrawer',
     'ComponentsSocialMedia'
   ]
-}, options)
+})
 
 describe('Blocks / Drawer', () => {
-  beforeAll(() => {
-    JestMockMatchMedia()
+  describe('Unit', () => {
+    shallPassUnitSanityTest({
+      component: Drawer,
+      description: 'default',
+      options: defaultOptionsUnitFactory()
+    })
+
+    shallPassUnitSanityTest({
+      component: Drawer,
+      description: 'shallOpenDrawer = true',
+      options: defaultOptionsUnitFactory({
+        data () {
+          return {
+            shallOpenDrawer: true
+          }
+        }
+      })
+    })
+
+    test('shall show drawer when common/matchMedia/desktop/matches = true', async () => {
+      const wrapper = await createUnitTestWrapper({
+        component: Drawer,
+        options: defaultOptionsUnitFactory()
+      })
+
+      // @ts-ignore
+      wrapper.vm['common/matchMedia/desktop/onChange']({ matches: true } as MediaQueryListEvent)
+      await wrapper.vm.$nextTick()
+      expect(wrapper.html()).toMatchSnapshot()
+
+      wrapper.destroy()
+    })
+
+    test('shall show & hide drawer on "blocks/drawer/toggleDrawer"', async () => {
+      const wrapper = await createUnitTestWrapper({
+        component: Drawer,
+        options: defaultOptionsUnitFactory()
+      })
+
+      wrapper.vm.$root.$emit('blocks/drawer/toggleDrawer')
+      await wrapper.vm.$nextTick()
+      expect(wrapper.html()).toMatchSnapshot()
+
+      wrapper.vm.$root.$emit('blocks/drawer/toggleDrawer')
+      await wrapper.vm.$nextTick()
+      expect(wrapper.html()).toMatchSnapshot()
+
+      wrapper.destroy()
+    })
   })
 
-  shallRender(Drawer, defaultOptionsFactory())
+  describe('Integration', () => {
+    shallPassIntegrationSanityTest({
+      component: Drawer
+    })
+
+    test('shall hide drawer on navigation-item click', async () => {
+      const wrapper = await createIntegrationTestWrapper({
+        component: Drawer,
+        options: {
+          data () {
+            return {
+              shallOpenDrawer: true
+            }
+          }
+        }
+      })
+
+      wrapper.get('[data-test="navigation-item"]').trigger('click')
+      await wrapper.vm.$nextTick()
+      expect(wrapper.html()).toMatchSnapshot()
+
+      wrapper.destroy()
+    })
+  })
 })
