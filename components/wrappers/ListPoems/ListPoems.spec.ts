b10c827c355c07ebf79157b15f26711a4e249896
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-02 14:57:58 +0100

    test: [#306] list poems wrapper

--- components/wrappers/ListPoems/ListPoems.spec.ts
+++ components/wrappers/ListPoems/ListPoems.spec.ts
@@ -1,91 +1,121 @@
-import merge from 'lodash/merge'
-import { mount } from '@vue/test-utils'
-import ListPoems from './ListPoems.vue'
+import ListPoems from '@/components/wrappers/ListPoems/ListPoems.vue'
+import { shallPassUnitSanityTest } from '@/devtools/jest.common.spec'
+import { createDefaultOptionsFactory, createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
 import { JestMockNuxtContent } from '@/devtools/jest.mock.nuxt.content'
 
 jest.mock('@/utils/dto.meta.post.poem', () => ({
-  DTOMetaPostPoem: class MockEmptyClass {}
+  DTOMetaPostPoem: class DTOMetaPostPoem {}
 }))
 
-const defaultOptionsFactory = (options?: object) => merge({
+const defaultOptionsFactory = createDefaultOptionsFactory({
   mocks: {
     $content: JestMockNuxtContent(Array(10).fill({}))
+  },
+  slots: {
+    default: 'This is slot "default" test.'
   }
-}, options)
+})
 
 describe('Wrappers / List Poems', () => {
-  test('shall render', async (): Promise<void> => {
-    const wrapper = mount(ListPoems, defaultOptionsFactory())
-    await (ListPoems as any).options.fetch.call(wrapper.vm)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  test('shall have slot default', async (): Promise<void> => {
-    const wrapper = mount(ListPoems, defaultOptionsFactory({
-      slots: {
-        default: 'This is slot "default" test.'
-      }
-    }))
-    await (ListPoems as any).options.fetch.call(wrapper.vm)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  test('shall { metaPostPoems } be Array<DTOMetaPostPoem>', async (): Promise<void> => {
-    const wrapper = mount(ListPoems, defaultOptionsFactory())
-    await (ListPoems as any).options.fetch.call(wrapper.vm)
-    const metaPostPoemsConstructorNames = (wrapper.vm as any).metaPostPoems.map((e: any) => e.constructor.name)
-    expect(metaPostPoemsConstructorNames.every((e: string) => e === 'MockEmptyClass')).toBe(true)
-  })
-
-  describe('Randomize: false', (): void => {
-    test('shall { metaPostPoems } be Array fetch output Object', async (): Promise<void> => {
-      const wrapper = mount(ListPoems, defaultOptionsFactory({
-        mocks: {
-          $content: JestMockNuxtContent({})
-        }
-      }))
-      await (ListPoems as any).options.fetch.call(wrapper.vm)
-      expect(Array.isArray((wrapper.vm as any).metaPostPoems)).toBe(true)
+  describe('Unit', () => {
+    shallPassUnitSanityTest({
+      component: ListPoems,
+      options: defaultOptionsFactory()
     })
 
-    test('shall $content.limit() be called with 3 when limit = 3', async (): Promise<void> => {
-      const mockNuxtContent = JestMockNuxtContent(Array(10).fill({}))
-      const wrapper = mount(ListPoems, defaultOptionsFactory({
-        propsData: {
-          limit: 3
-        },
-        mocks: {
-          $content: mockNuxtContent
-        }
-      }))
-      await (ListPoems as any).options.fetch.call(wrapper.vm)
-      expect(mockNuxtContent().limit).toHaveBeenCalledWith(3)
-    })
-  })
+    describe('Randomize: false', () => {
+      test('shall { metaPostPoems } match snapshot', async () => {
+        const wrapper = await createUnitTestWrapper({
+          component: ListPoems,
+          options: defaultOptionsFactory()
+        })
+        // @ts-ignore
+        expect(wrapper.vm.metaPostPoems).toMatchSnapshot()
+        wrapper.destroy()
+      })
 
-  describe('Randomize: true', (): void => {
-    test('shall { metaPostPoems } be Array fetch output Object', async (): Promise<void> => {
-      const wrapper = mount(ListPoems, defaultOptionsFactory({
-        mocks: {
-          $content: JestMockNuxtContent({})
-        },
-        propsData: {
-          randomize: true
-        }
-      }))
-      await (ListPoems as any).options.fetch.call(wrapper.vm)
-      expect(Array.isArray((wrapper.vm as any).metaPostPoems)).toBe(true)
+      test('shall $content.limit() be called with 3 when limit = 3', async () => {
+        const MockNuxtContent = JestMockNuxtContent(Array(1).fill({}))
+        const wrapper = await createUnitTestWrapper({
+          component: ListPoems,
+          options: defaultOptionsFactory({
+            mocks: {
+              $content: MockNuxtContent
+            },
+            propsData: {
+              limit: 3
+            }
+          })
+        })
+        expect(MockNuxtContent().limit).toHaveBeenCalledWith(3)
+        wrapper.destroy()
+      })
+
+      test('shall $content.where() be called with "/mock-path/" when without = "/mock-path/"', async () => {
+        const MockNuxtContent = JestMockNuxtContent(Array(1).fill({}))
+        const wrapper = await createUnitTestWrapper({
+          component: ListPoems,
+          options: defaultOptionsFactory({
+            mocks: {
+              $content: MockNuxtContent
+            },
+            propsData: {
+              without: ['/mock-path/']
+            }
+          })
+        })
+        expect(MockNuxtContent().where).toHaveBeenCalledWith({ path: { $nin: ['/mock-path/'] } })
+        wrapper.destroy()
+      })
     })
 
-    test('shall { metaPostPoems } be Array.length = 3 when limit = 3', async (): Promise<void> => {
-      const wrapper = mount(ListPoems, defaultOptionsFactory({
-        propsData: {
-          limit: 3,
-          randomize: true
-        }
-      }))
-      await (ListPoems as any).options.fetch.call(wrapper.vm)
-      expect((wrapper.vm as any).metaPostPoems).toHaveLength(3)
+    describe('Randomize: true', () => {
+      test('shall { metaPostPoems } match snapshot', async () => {
+        const wrapper = await createUnitTestWrapper({
+          component: ListPoems,
+          options: defaultOptionsFactory({
+            propsData: {
+              randomize: true
+            }
+          })
+        })
+        // @ts-ignore
+        expect(wrapper.vm.metaPostPoems).toMatchSnapshot()
+        wrapper.destroy()
+      })
+
+      test('shall $content.limit() be called with 3 when limit = 3', async () => {
+        const wrapper = await createUnitTestWrapper({
+          component: ListPoems,
+          options: defaultOptionsFactory({
+            propsData: {
+              limit: 3,
+              randomize: true
+            }
+          })
+        })
+        // @ts-ignore
+        expect(wrapper.vm.metaPostPoems).toHaveLength(3)
+        wrapper.destroy()
+      })
+
+      test('shall $content.where() be called with "/mock-path/" when without = "/mock-path/"', async () => {
+        const MockNuxtContent = JestMockNuxtContent(Array(1).fill({}))
+        const wrapper = await createUnitTestWrapper({
+          component: ListPoems,
+          options: defaultOptionsFactory({
+            mocks: {
+              $content: MockNuxtContent
+            },
+            propsData: {
+              randomize: true,
+              without: ['/mock-path/']
+            }
+          })
+        })
+        expect(MockNuxtContent().where).toHaveBeenCalledWith({ path: { $nin: ['/mock-path/'] } })
+        wrapper.destroy()
+      })
     })
   })
 })
