82e427c155a7e0952b928c9aa3a255c6dca38042
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-05 01:53:04 +0100

    feat: scroll behavior nuxt unstable scrolling fix

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,3 +1,15 @@
+/**
+ * So, this is hacky one as hell, but without "await nuxt.$nextTick()" position from resolved was ignored ¯\_(ツ)_/¯
+ * Yep, this Promise / async-await mix looks "interesting", but it works like a charm ;)
+ */
 export default function (_to, _from, savedPosition) {
-  return savedPosition || { x: 0, y: 0 }
+  const nuxt = window['<%= globals.nuxt %>']
+  const position = savedPosition || { x: 0, y: 0 }
+
+  return new Promise((resolve) => {
+    nuxt.$root.$once('triggerScroll', async () => {
+      await nuxt.$nextTick()
+      resolve(position)
+    })
+  })
 }
