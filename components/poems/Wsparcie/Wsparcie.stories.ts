7b16810c778fef2e7a111767282efb057f095d59
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-05 23:46:42 +0100

    refactor: [#304] wsparcie poem

--- components/poems/Wsparcie/Wsparcie.stories.ts
+++ components/poems/Wsparcie/Wsparcie.stories.ts
@@ -1,13 +1,21 @@
-import { Meta, Story } from '@storybook/vue'
-import Wsparcie from './Wsparcie.vue'
+import type { Meta, Story } from '@storybook/vue'
+import PoemsWsparcie from '@/components/poems/Wsparcie/Wsparcie.vue'
+import WrapperTextPoems from '@/components/wrappers/TextPoems/TextPoems.vue'
 
 const meta: Meta = {
   title: 'Poems / Wsparcie',
-  component: Wsparcie
+  component: PoemsWsparcie
 }
 export default meta
 
 export const Default: Story = () => ({
-  components: { Wsparcie },
-  template: '<Wsparcie />'
+  components: {
+    PoemsWsparcie,
+    WrapperTextPoems
+  },
+  template: `<div class="__storybook-section__" style="background-color: var(--color-background-1)">
+    <WrappersTextPoems>
+      <PoemsWsparcie />
+    </WrappersTextPoems>
+  </div>`
 })
