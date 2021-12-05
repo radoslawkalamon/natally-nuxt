5e15e2aaba25be81df9c49ba105db6848dfe09c7
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-05 23:16:21 +0100

    refactor: [#304] list stories main block

--- components/blocks/ListStoriesMain/ListStoriesMain.stories.ts
+++ components/blocks/ListStoriesMain/ListStoriesMain.stories.ts
@@ -1,13 +1,18 @@
-import { Meta, Story } from '@storybook/vue'
-import ListStoriesMain from './ListStoriesMain.vue'
+import type { Meta, Story } from '@storybook/vue'
+import BlocksListStoriesMain from '@/components/blocks/ListStoriesMain/ListStoriesMain.vue'
+import { storybookOnlyDevTemplate } from '@/devtools/storybook.onlyDev.template'
 
 const meta: Meta = {
   title: 'Blocks / List Stories Main',
-  component: ListStoriesMain
+  component: BlocksListStoriesMain
 }
 export default meta
 
 export const Default: Story = () => ({
-  components: { ListStoriesMain },
-  template: '<div class="__storybook-section__"><ListStoriesMain /></div>'
+  components: {
+    BlocksListStoriesMain
+  },
+  template: process.env.NODE_ENV !== 'production'
+    ? '<div class="__storybook-section__"><BlocksListStoriesMain /></div>'
+    : storybookOnlyDevTemplate
 })
