852198b15ef7314f31fd004b322b642a4ea90119
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-05 23:04:46 +0100

    refactor: [#304] header block

--- components/blocks/Header/Header.stories.ts
+++ components/blocks/Header/Header.stories.ts
@@ -1,5 +1,6 @@
-import { Meta, Story } from '@storybook/vue'
-import BlockHeader from './Header.vue'
+import type { Meta, Story } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+import BlockHeader from '@/components/blocks/Header/Header.vue'
 
 const meta: Meta = {
   title: 'Blocks / Header',
@@ -8,9 +9,35 @@ const meta: Meta = {
 export default meta
 
 export const Default: Story = () => ({
-  components: { BlockHeader },
-  template:
-  `<div style="background-color: var(--color-background-1); height: 2000px">
-    <BlockHeader />
-  </div>`
+  beforeMount () {
+    // @ts-ignore
+    this.$root.$on('blocks/drawer/toggleDrawer', this.onToggleDrawer)
+  },
+  created () {
+    // @ts-ignore
+    this.$root.$on('blocks/drawer/toggleDrawer', action('blocks/drawer/toggleDrawer'))
+    // @ts-ignore
+    this.$root.$on('blocks/drawer/syncDrawerState', action('blocks/drawer/syncDrawerState'))
+  },
+  components: {
+    BlockHeader
+  },
+  data () {
+    return {
+      isDrawerOpen: false
+    }
+  },
+  methods: {
+    onToggleDrawer () {
+      // @ts-ignore
+      this.isDrawerOpen = !this.isDrawerOpen
+    }
+  },
+  template: '<div style="background-color: var(--color-background-1); height: 2000px"><BlockHeader /></div>',
+  watch: {
+    isDrawerOpen (v: boolean) {
+      // @ts-ignore
+      this.$root.$emit('blocks/drawer/syncDrawerState', v)
+    }
+  }
 })
