2f273d5d01f07586eb61251475e4b152b5c72872
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-05 23:23:59 +0100

    refactor: [#304] page meta block

--- components/blocks/PageMeta/PageMeta.stories.ts
+++ components/blocks/PageMeta/PageMeta.stories.ts
@@ -1,20 +1,33 @@
-import { Meta, Story } from '@storybook/vue'
-import PageMeta from './PageMeta.vue'
+import type { Meta, Story } from '@storybook/vue'
+import BlocksPageMeta from '@/components/blocks/PageMeta/PageMeta.vue'
 
 const meta: Meta = {
   title: 'Blocks / Page Meta',
-  component: PageMeta
+  component: BlocksPageMeta,
+  argTypes: {
+    createdAt: {
+      control: { type: 'date' }
+    }
+  }
 }
 export default meta
 
 const Template: Story = (_args, { argTypes }) => ({
-  components: { PageMeta },
+  components: {
+    BlocksPageMeta
+  },
   props: Object.keys(argTypes),
-  template: '<PageMeta v-bind="$props" />'
+  template: '<BlocksPageMeta v-bind="$props" />'
 })
 
-export const Default: Story = Template.bind({})
-Default.args = {
-  datePublished: '01.05.2019',
+export const NoDate: Story = Template.bind({})
+NoDate.args = {
+  createdAt: new Date(0),
+  title: 'Idealny moment nie istnieje'
+}
+
+export const WithDate: Story = Template.bind({})
+WithDate.args = {
+  createdAt: new Date(),
   title: 'Idealny moment nie istnieje'
 }
