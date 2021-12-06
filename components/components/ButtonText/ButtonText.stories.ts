5cbed0ecec8bb63fb305c350fc5974db1c1a2d1e
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-06 14:29:00 +0100

    refactor: [#304] button text component

--- components/components/ButtonText/ButtonText.stories.ts
+++ components/components/ButtonText/ButtonText.stories.ts
@@ -1,17 +1,19 @@
-import { Meta, Story } from '@storybook/vue'
+import type { Meta, Story } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
-import ButtonText from '@/components/components/ButtonText/ButtonText.vue'
+import ComponentsButtonText from '@/components/components/ButtonText/ButtonText.vue'
 
 const meta: Meta = {
   title: 'Components / Button Text',
-  component: ButtonText
+  component: ComponentsButtonText
 }
 export default meta
 
 const Template: Story = (_args, { argTypes }) => ({
-  components: { ButtonText },
+  components: {
+    ComponentsButtonText
+  },
   props: Object.keys(argTypes),
-  template: '<ButtonText v-bind="$props" @click="onClick" />',
+  template: '<ComponentsButtonText v-bind="$props" @click="onClick" />',
   methods: {
     onClick () {
       action('Click')()
