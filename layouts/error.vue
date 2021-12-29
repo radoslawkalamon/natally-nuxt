3a3e43aecfa770ebd8832b1eabfc410b39adff7a
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-30 00:28:21 +0100

    fix: [#338] no nuxt/fetch on fallback page

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,8 +2,6 @@
   <article class="page-error">
     <BlocksAnimationPufferfish />
     <BlocksPageMeta404 />
-    <BlocksListStoriesSuggestions />
-    <BlocksListPoemsSuggestions />
   </article>
 </template>
 
@@ -12,17 +10,13 @@ import Vue from 'vue'
 import type { MetaInfo } from 'vue-meta'
 import BlocksAnimationPufferfish from '@/components/blocks/AnimationPufferfish/AnimationPufferfish.vue'
 import BlocksPageMeta404 from '@/components/blocks/PageMeta404/PageMeta404.vue'
-import BlocksListPoemsSuggestions from '@/components/blocks/ListPoemsSuggestions/ListPoemsSuggestions.vue'
-import BlocksListStoriesSuggestions from '@/components/blocks/ListStoriesSuggestions/ListStoriesSuggestions.vue'
 import { factoryHeadPageNotfound } from '@/utils/factory.head.page.notfound'
 
 export default Vue.extend({
   name: 'Page404',
   components: {
     BlocksAnimationPufferfish,
-    BlocksPageMeta404,
-    BlocksListPoemsSuggestions,
-    BlocksListStoriesSuggestions
+    BlocksPageMeta404
   },
   head (): MetaInfo {
     return factoryHeadPageNotfound()
