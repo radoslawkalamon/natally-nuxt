cf6a9df4fa81b32692c2ae5c48205152957ab300
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-12-30 00:17:04 +0100

    test: [#338] added head test

--- layouts/error.spec.ts
+++ layouts/error.spec.ts
@@ -1,11 +1,17 @@
 import Error from '@/layouts/error.vue'
-import { shallPassIntegrationSanityTest } from '@/devtools/jest.common.spec'
+import { expectHeadMatchSnapshot, shallPassIntegrationSanityTest } from '@/devtools/jest.common.spec'
+import { createUnitTestWrapper } from '@/devtools/jest.common.spec.utils'
 
 describe('Page / Error', () => {
-  describe('Integration', () => {
-    shallPassIntegrationSanityTest({
-      component: Error,
-      options: {}
+  describe('Unit', () => {
+    test('shall head match snapshot', async () => {
+      const wrapper = await createUnitTestWrapper({ component: Error })
+      expectHeadMatchSnapshot({ wrapper })
+      wrapper.destroy()
     })
+  })
+
+  describe('Integration', () => {
+    shallPassIntegrationSanityTest({ component: Error })
   })
 })
