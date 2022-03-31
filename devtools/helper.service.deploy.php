937a041df608bf7518d81c0094d48d19d469bf18
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2022-03-31 20:40:54 +0200

    feat: deploy helper [#379]

---
+++ devtools/helper.service.deploy.php
@@ -0,0 +1,18 @@
+<?php
+
+$script_path = __FILE__;
+$script_name = basename($script_path, '.php');
+$script_dir = dirname(__FILE__);
+$archive_path = $script_dir.'/'.$script_name.'.zip';
+
+try {
+  $zip = new ZipArchive();
+  $zip->open($archive_path);
+  $zip->extractTo('.');
+  $zip->close();
+} catch (Exception $e) {
+
+} finally {
+  unlink($archive_path);
+  unlink($script_filepath);
+}
