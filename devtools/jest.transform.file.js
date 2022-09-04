98d0184e9a51326cd7c62b7aa1cc7f66107cd26c
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2022-09-04 14:01:51 +0200

    chore: jest / cypress conflict resolved

--- devtools/jest.transform.file.js
+++ devtools/jest.transform.file.js
@@ -2,6 +2,8 @@ const path = require('path')
 
 module.exports = {
   process (_src, filename, _config, _options) {
-    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';'
+    return {
+      code: 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';'
+    }
   }
 }
--- jest.config.ts
+++ jest.config.ts
@@ -12,8 +12,8 @@ export default {
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/devtools/jest.transform.file.js'
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/devtools/jest.transform.file.js'
   },
   collectCoverage: true,
   collectCoverageFrom: [
