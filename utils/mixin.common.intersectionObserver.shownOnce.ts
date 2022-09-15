5319623ebf4c77d8603667c316325e65c20f00ee
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2022-09-15 23:05:13 +0200

    feat: add cache for shownOnce [#393]

--- utils/mixin.common.intersectionObserver.shownOnce.ts
+++ utils/mixin.common.intersectionObserver.shownOnce.ts
@@ -1,26 +1,52 @@
 import Vue from 'vue'
 
+const CacheSet = new Set()
+
 export default Vue.extend({
   data () {
     return {
       'common/intersectionObserver/shownOnce': false
     }
   },
-  mounted (): void {
-    const intersectionObserverInit: IntersectionObserverInit = {
-      rootMargin: '160px 0px'
+  computed: {
+    'common/intersectionObserver/cacheKey' (): string {
+      return ''
+    },
+    'common/intersectionObserver/shallUseCache' (): boolean {
+      return CacheSet.has(this['common/intersectionObserver/cacheKey'])
     }
-    const intersectionObserver = new IntersectionObserver(
-      this['common/intersectionObserver/observerCallback'],
-      intersectionObserverInit
-    )
-    intersectionObserver.observe(this.$el)
+  },
+  created (): void {
+    if (this['common/intersectionObserver/shallUseCache']) {
+      this['common/intersectionObserver/setShownOnce']()
+    }
+  },
+  mounted (): void {
+    if (!this['common/intersectionObserver/shallUseCache']) {
+      const intersectionObserverInit: IntersectionObserverInit = {
+        rootMargin: '160px 0px'
+      }
+      const intersectionObserver = new IntersectionObserver(
+        this['common/intersectionObserver/observerCallback'],
+        intersectionObserverInit
+      )
+      intersectionObserver.observe(this.$el)
+    }
   },
   methods: {
+    'common/intersectionObserver/setShownOnce' () {
+      this['common/intersectionObserver/shownOnce'] = true
+    },
+    'common/intersectionObserver/writeCacheKey' () {
+      CacheSet.add(this['common/intersectionObserver/cacheKey'])
+    },
     'common/intersectionObserver/observerCallback' (entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
       entries.forEach((entry: IntersectionObserverEntry) => {
         if (entry.isIntersecting) {
-          this['common/intersectionObserver/shownOnce'] = true
+          if (this['common/intersectionObserver/cacheKey'] !== '') {
+            this['common/intersectionObserver/writeCacheKey']()
+          }
+          this['common/intersectionObserver/setShownOnce']()
           observer.disconnect()
         }
       })
